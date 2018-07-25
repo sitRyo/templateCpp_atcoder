48836670aaad90003df3621481be0988eb594a0b
Ryo <>
2018-07-26 00:39:56 +0900

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <numeric>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+#define INF 10e10
+#define rep(i,n) for(int i=0; i<n; i++)
+#define rep_r(i,n,m) for(int i=m; i<n; i++)
+#define MAX 100
+
+/* add vars here */
+
+/****************/
+
+/* add your algorithm here */
+
+int main() {
+
+}
