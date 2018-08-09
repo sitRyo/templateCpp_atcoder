d4f66d29fd50de6c23e0c2957458b666ddab7618
sitRyo <al15030@shibaura-it.ac.jp>
2018-08-09 22:57:51 +0900

    400点問題に着手

--- Atcoder_400/ABC57_D.cpp
+++ Atcoder_400/ABC57_D.cpp
@@ -13,7 +13,6 @@ using namespace std;
 
 typedef long long ll;
 
-#define INF 10e10
 #define rep(i,n) for(int i=0; i<n; i++)
 #define rep_r(i,n,m) for(int i=m; i<n; i++)
 #define MAX 100
@@ -22,9 +21,24 @@ typedef long long ll;
 
 /* add vars here */
 
+int n,a,b;
+ll v[51];
+ll ans = 0, mmax = 0;
+ll dp[51][51];
 
 /* add your algorithm here */
 
 int main() {
+  cin >> n >> a >> b;
+  for (int i = 1; i <= n; ++i) cin >> v[i];
 
+  dp[0][0] = 0;
+
+  for (int )
+
+  for (int i = 1; i < b; ++i) {
+    for (int j = 0; j < i; ++j) {
+      dp[i+1][j+1] = max(dp[i][j] + v[i], dp[i][j+1]);
+    }
+  }
 }
